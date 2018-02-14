3c4ee4943303be52254b782297ed94d7238ad5b5
Kurt Strecker <ksphotopro@gmail.com>
2018-02-14 00:54:17 -0500

    changed store list to dict

--- database_seed.py
+++ database_seed.py
@@ -1,7 +1,9 @@
 import csv
 import os
 
+
 def build_path(filename):
+    '''lets you use a relative path to the data, rather than requiring absolute'''
     script_dir = os.path.dirname(__file__)
     rel_path = filename
     abs_file_path = os.path.join(script_dir, rel_path)
@@ -9,6 +11,8 @@ def build_path(filename):
     return abs_file_path
 
 def read_csv(filename, year):
+    '''a generator function that reads through the CSV file,
+    yielding a single row as needed'''
     print('Generator Initiated')
     with open(filename, 'r') as raw_data:
         data_reader = csv.reader(raw_data)
@@ -21,20 +25,21 @@ def read_csv(filename, year):
     print('Generator Complete')
 
 def batch_stores_output(year):
-    output = []
+    '''the for loop in this function checks against some condition, in this case Store Number
+    and adds the store details to a dictionary (key = Store Number)'''
+    output = {}
     raw_data_generator = read_csv(abs_path, year)
 
     for row in raw_data_generator:
-        if not any(row[2] in sl for sl in output):
+        if not row[2] in output:
+            output[row[2]] = row[2:10]
             print('added {} to store list'.format(row[3]))
-            output.append(row[2:10])
     return output
     
 abs_path = build_path(r'iowa-liquor-sales\Iowa_Liquor_Sales.csv')
-annual_sale = batch_stores_output('2015')
+all_unique_stores = batch_stores_output('2015')
 
-
-print(annual_sale[-1])
-print(len(annual_sale))
+# print(annual_sale[-1])
+print(len(all_unique_stores))
 
 
